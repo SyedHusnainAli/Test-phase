d7fcdc0615ebe5df42001e8a01c14e8a1457b0c2
SyedHusnainAli <syedHusnain1200@gmail.com>
2022-10-20 10:11:51 -0700

    create_price_Card

--- src/navbar-component/pricing.jsx
+++ src/navbar-component/pricing.jsx
@@ -1,17 +1,71 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import '../styles/pricing.css';
 
 function Pricing(){
 
+const price = [
+    {
+    value: 'Per Month',
+    value2: '$50',
+    value3: 'Basic',
+},
+{
+    value: 'Per Month',
+    value2: '$90',
+    value3: 'Standard'
+},
+{
+    value: 'Per Month',
+    value2: '$30',
+    value3: 'InterShip'
+},
+{
+    value: 'Per Month',
+    value2: '$250',
+    value3: 'Project'
+},
+]
     return(
-        <div>
-               <h1>pricing its</h1>
-               <li>
-              <Link to={"/pricing"} >Pricing</Link>
-             </li>
-        </div>
-    )
+  <>  
+         
+<div className="main-div">
+   
+            <div className="text-div">
+               <h1>Premium Plans</h1>
+               <h3>1 month free not available for users who have already tried Premium. 1 month free!</h3>
+               </div>
+              <div className="container_div">
+              {price.map(function(into){
+          return(
+                 <div className="container-1">
+                    <h4> {into.value}</h4>
+                    <h1>{into.value2}</h1>
+                    <h3>{into.value3}</h3>
+                    <hr />
+                    <hr />
+                    <hr />
+                    <ul className="point">
+                        <li> lipsum  sometimes known</li>
+                        <li> lipsum sometimes known</li>
+                        <li> lipsum  sometimes known</li>
+                        <li> lipsum sometimes known</li>
+                        <li> lipsum sometimes known</li>
+                        <li> lipsum  sometimes known</li>
+                        <li> lipsum sometimes known</li>
+                    </ul>
+                    <button className="btn-click"  >Click Here</button>
+                 </div>
+                          )})
+                        }
+              </div>
+
+        </div>        
+ 
+        </>
+
+          )
+    
 }
+
 
 export default Pricing;
